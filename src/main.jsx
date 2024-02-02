50420770f95c5cac9488264cd31372811266ff8e
nickonyi <nickoluoch1130@gmail.com>
2024-02-02 06:26:43 +0300

    bounty

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Gallery from './App.jsx'
+import Person from './Person.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Gallery />
+    <Person />
   </React.StrictMode>,
 )
